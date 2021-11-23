7d6ea408de3ee037c0c96a13b13f02f6f0bc3fb0
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2021-11-24 04:48:54 +0800

    init

---
+++ Novepus/src/main/java/com/project/novepus/NovepusApplication.java
@@ -0,0 +1,13 @@
+package com.project.novepus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovepusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovepusApplication.class, args);
+    }
+
+}
---
+++ Novepus/src/test/java/com/project/novepus/NovepusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.novepus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovepusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
